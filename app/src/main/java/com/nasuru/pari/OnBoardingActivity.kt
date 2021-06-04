54cd1d1e8bf9b612db32b5985347730e8c0a9092
harisnasrull <harisnasrullah212@gmail.com>
2021-06-04 21:03:07 +0700

    add viewBinding, add onboarding adapter, update onboarding activity, update splash screen

--- app/src/main/java/com/nasuru/pari/OnBoardingActivity.kt
+++ app/src/main/java/com/nasuru/pari/OnBoardingActivity.kt
@@ -1,11 +1,125 @@
 package com.nasuru.pari
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.nasuru.pari.adapter.OnBoardingAdapter
+import com.nasuru.pari.databinding.ActivityOnBoardingBinding
+import com.nasuru.pari.model.OnBoardingItem
 
 class OnBoardingActivity : AppCompatActivity() {
+    private lateinit var onBoardingAdapter: OnBoardingAdapter
+    private lateinit var binding: ActivityOnBoardingBinding
+    private lateinit var indicatorTab: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setOnBoardingItems()
+        setupIndicators()
+        setCurrentIndicator(0)
+    }
+
+    private fun setOnBoardingItems() {
+        onBoardingAdapter = OnBoardingAdapter(
+            listOf(
+                OnBoardingItem(
+                    image = R.drawable.oboarding1,
+                    title = "Aplikasi Untuk Petani",
+                    desc = "Melalui aplikasi  ini kami ingin \n" +
+                            "membantu petani padi untuk \n" +
+                            "mendeteksi lebih awal permasalahan \n" +
+                            "pada tanaman mereka",
+                ),
+                OnBoardingItem(
+                    image = R.drawable.oboarding2,
+                    title = "Cukup foto sehelai daun",
+                    desc = "Ambil foto sampel daun padi\n" +
+                            "dan aplikasi akan memrosesnya\n" +
+                            "secara otomatis",
+                )
+            )
+        )
+        val onBoardingViewPager = binding.onboardingViewPager
+        onBoardingViewPager.adapter = onBoardingAdapter
+        onBoardingViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicator(position)
+            }
+        })
+        (onBoardingViewPager.getChildAt(0) as RecyclerView).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+        binding.buttonNext.setOnClickListener {
+            if (onBoardingViewPager.currentItem + 1 < onBoardingAdapter.itemCount) {
+                onBoardingViewPager.currentItem += 1
+            }
+            else {
+                navigateMain()
+            }
+        }
+        binding.buttonSkip.setOnClickListener {
+            navigateMain()
+        }
+    }
+
+    private fun setupIndicators() {
+        indicatorTab = binding.indicator
+        val indicators = arrayOfNulls<ImageView>(onBoardingAdapter.itemCount)
+        val layoutParameter: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        layoutParameter.setMargins(0, 0, 0, 0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i]?.let {
+                it.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive
+                    )
+                )
+                it.layoutParams = layoutParameter
+                indicatorTab.addView(it)
+            }
+        }
+    }
+
+    private fun setCurrentIndicator(position: Int) {
+        val childCount = indicatorTab.childCount
+        for (i in 0 until childCount) {
+            val imageView = indicatorTab.getChildAt(i) as ImageView
+            if (i == position) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active
+                    )
+                )
+            }
+            else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive
+                    )
+                )
+            }
+        }
+    }
+
+    private fun navigateMain(){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/nasuru/pari/adapter/OnBoardingAdapter.kt
+++ app/src/main/java/com/nasuru/pari/adapter/OnBoardingAdapter.kt
@@ -1,4 +1,37 @@
 package com.nasuru.pari.adapter
 
-class OnBoardingAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.nasuru.pari.databinding.OnboardingItemBinding
+import com.nasuru.pari.model.OnBoardingItem
+
+class OnBoardingAdapter(private val onBoardingItem: List<OnBoardingItem>) :
+    RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
+
+
+    inner class OnBoardingViewHolder(val binding: OnboardingItemBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(onBoardingItem: OnBoardingItem): Unit {
+            binding.apply {
+                binding.onboardingTitle.text = onBoardingItem.title
+                binding.onboardingImage.setImageResource(onBoardingItem.image)
+                binding.onboardingDesc.text = onBoardingItem.desc
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder {
+        val view = OnboardingItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return OnBoardingViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: OnBoardingViewHolder, position: Int) {
+        holder.bind(onBoardingItem[position])
+    }
+
+    override fun getItemCount(): Int = onBoardingItem.size
 }
